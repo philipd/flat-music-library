ca50e329c5bea857f67c28057f80d8dbc7995ecb
philipd <philip.hd@protonmail.com>
2020-11-18 09:45:04 -0700

    Implement function to check if file is an mp3

--- index.js
+++ index.js
@@ -2,13 +2,55 @@ const fs = require('fs');
 const uuid = require('uuid').v4;
 const sh = require('shelljs');
 const json = JSON;
+const walk = require('walk');
+
+const isMp3 = function(filepath) {
+  return filepath.substring(filepath.length-3, filepath.length) === 'mp3';
+}
+
+const hasGuid = function(filepath) {
+  return true;
+}
+
+const linkExists = function(filepath) {
+  return true;
+}
 
 
 const config = json.parse(fs.readFileSync('config.json'));
 const dir = config.taggerDirectory;
+const options = {
+  // followLinks: false,
+  filters: ["Temp", "_Temp"]
+};
+
+console.log(dir);
+
+const walker = walk.walk(dir, options);
+
+// walker.on("name", (root, nodeNamesArray) => {
+// });
+
+walker.on("file", (root, fileStats, next) => {
+  console.log(root+fileStats.name);
+  console.log(isMp3(fileStats.name));
+  next();
+
+  // file is an mp3?
+    // continue
+  // check for guid tag
+      // no guid tag? add one
+  // check for link
+      // 2. file is an mp3
+  // create a link
+});
+
+walker.on("end", function () {
+  console.log("all done");
+});
 
 // sh.ln('--', 'test/testfile', dir+uuid());
 
-for (let item of sh.ls(dir)) {
-  console.log(item);
-}
+// for (let item of sh.ls(dir)) {
+//   console.log(item);
+// }
--- plans.md
+++ plans.md
@@ -6,7 +6,7 @@ Tagger is a way to organize your music library that gives you the freedom to mod
 
 It creates a flat folder with GUID-named hardlinks to your original files. You can then import the files into whichever music library software you like.
 
-The original files are modified with the addition of the GUID to the id3 tags. This allows you to rebuild the hardlinks if they are broken or lost.
+The original mp3 files are modified with the addition of the GUID to the id3 tags. This allows you to rebuild the hardlinks if they are broken or lost.
 
 Before creating a hardlink, it checks if one already exists with the same inode. If it does, then a new hardlink is not created.
 
